bcff0cbf81eee312c5d933b1d2a0054310024b7c
Aura Zelco <aura.zelco@gmail.com>
2022-11-08 20:44:45 +0100

    Updates UCSC scripts

--- UCSC/split_big_ExprMtx.sh
+++ UCSC/split_big_ExprMtx.sh
@@ -18,6 +18,7 @@ tmp=$(mktemp)
 
 
 # for each CSV file in the input directory
+
 for file in $bi_directory"/"*.txt; do
   # removes path so only filename is saved - used for output names
   id=${file%.txt}
@@ -25,9 +26,11 @@ for file in $bi_directory"/"*.txt; do
   # prints the filename, so we can keep track which file is being analyzed
   echo $id
   # sets the output names for the GRN and CTX softwares as string variables
-  indexes=$(cat $file)
-  out_name="$out$id".tsv
-  gunzip -c $input_TSV_file | cut -f$indexes > "$tmp" 
-  gzip -c "$tmp" >  $out_name 
+  #indexes=$(cat $file)
+  indexes=$(<$file)
+  #declare -i $indexes
+  out_name="$out"/"$id".tsv
+  echo $out_name
+  gunzip -c $input_TSV_file | cut -f$indexes | gzip > $out_name
 done
 
