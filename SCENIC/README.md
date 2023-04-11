8162e4448e7f30e2e2f65ee59f736fe75e722bac
Aura Zelco <aura.zelco@gmail.com>
2023-04-11 10:05:28 +0200

    Removes unused scripts and fixes PubMed publication script

--- SCENIC/README.md
+++
@@ -1,1 +0,0 @@
-* [check_SCENIC_results_func.R](SCENIC/check_SCENIC_results_func.R) - source file for checking SCENIC results
--- Thesis_draft/pubmed_results.R
+++ Thesis_draft/pubmed_results.R
@@ -15,43 +15,39 @@ library(ggplot2)
 library(ggpubr)
 `%!in%` <- Negate(`%in%`)
 
+files_path <- "/Users/aurazelco/Desktop/Lund_MSc/Thesis/scripts/Thesis_draft/extra_files/"
+dir.create("/Users/aurazelco/Desktop/Lund_MSc/Thesis/thesis_draft/images/", showWarnings = F, recursive = T)
 
-pubmed_all <- read.csv("/Users/aurazelco/Desktop/Lund_MSc/Thesis/scripts/Thesis_draft/extra_files/PubMed_Timeline_Results_by_Year.csv", sep=",", row.names = NULL)
-pubmed_all <- pubmed_all[-c(1,2),]
-colnames(pubmed_all) <- c("Year", "Count_all")
-str(pubmed_all)
-pubmed_all$Year <- as.numeric(pubmed_all$Year)
-pubmed_all$Count_all <- as.numeric(pubmed_all$Count_all)
-pubmed_all <- pubmed_all[order(pubmed_all$Year),]
+sexdiff_brain_all <- read.csv(paste0(files_path, "PubMed_Timeline_Results_by_Year_sexdiff_AND_brain.csv"), 
+                              sep=",", row.names = NULL, skip = 1)
+colnames(sexdiff_brain_all) <- c("Year", "Count_all")
+sexdiff_brain_all <- sexdiff_brain_all[order(sexdiff_brain_all$Year),]
 
-pubmed_dev <- read.csv("/Users/aurazelco/Desktop/Lund_MSc/Thesis/scripts/Thesis_draft/extra_files/PubMed_Timeline_Results_by_Year_development.csv", sep=",", row.names = NULL)
-pubmed_dev <- pubmed_dev[-c(1,2),]
-colnames(pubmed_dev) <- c("Year", "Count_dev")
-str(pubmed_dev)
-pubmed_dev$Year <- as.numeric(pubmed_dev$Year)
-pubmed_dev$Count_dev <- as.numeric(pubmed_dev$Count_dev)
-pubmed_dev <- pubmed_dev[order(pubmed_dev$Year),]
+sexdiff_brain_dev <- read.csv(paste0(files_path, "PubMed_Timeline_Results_by_Year_sexdiff_AND_brain_AND_dev.csv"), 
+                       sep=",", row.names = NULL, skip = 1)
+colnames(sexdiff_brain_dev) <- c("Year", "Count_dev")
+sexdiff_brain_dev <- sexdiff_brain_dev[order(sexdiff_brain_dev$Year),]
 
-missing_years <- pubmed_all[which(pubmed_all$Year %!in% pubmed_dev$Year), "Year"]
+missing_years <- sexdiff_brain_all[which(sexdiff_brain_all$Year %!in% sexdiff_brain_dev$Year), "Year"]
 
 missing_df <- data.frame(missing_years, rep(0, length(missing_years)))
 colnames(missing_df) <- c("Year", "Count_dev")
 
-pubmed_dev <- rbind(missing_df, pubmed_dev)
-pubmed_dev <- pubmed_dev[order(pubmed_dev$Year),]
+sexdiff_brain_dev <- rbind(missing_df, sexdiff_brain_dev)
+sexdiff_brain_dev <- sexdiff_brain_dev[order(sexdiff_brain_dev$Year),]
 
 
-pubmed <- merge(pubmed_all, pubmed_dev, by="Year", all.x=TRUE, all.y=TRUE)
-pubmed$adult <- pubmed$Count_all - pubmed$Count_dev
+pubmed <- merge(sexdiff_brain_all, sexdiff_brain_dev, by="Year", all.x=TRUE, all.y=TRUE)
+pubmed$non_dev <- pubmed$Count_all - pubmed$Count_dev
 pubmed <- reshape2::melt(pubmed[c(1, 3:4)], "Year")
 pubmed$Year <- as.factor(pubmed$Year)
-colnames(pubmed) <- c("year", "age", "count")
-levels(pubmed$age) <- c("development", "non-development")
+colnames(pubmed) <- c("year", "publ", "count")
+levels(pubmed$publ) <- c("Development", "Non-development")
 
-all <- ggplot(pubmed, aes(year, count, fill=age)) +
+all <- ggplot(pubmed, aes(year, count, fill=publ)) +
   geom_bar(stat="identity", color="black") +
-  scale_fill_manual(values = c("non-development"= "#D55E00", "development" = "#009E73")) +
-  labs(x="Year", y="Publications", fill="Age", title="A)") +
+  scale_fill_manual(values = c("Non-development"= "#D55E00", "Development" = "#009E73")) +
+  labs(x="Year", y="Publications", fill="Publications type", title="A)") +
   scale_x_discrete(breaks=seq(min(as.numeric(levels(pubmed$year))),
                               max(as.numeric(levels(pubmed$year))),
                               10)) +
@@ -60,16 +56,16 @@ all <- ggplot(pubmed, aes(year, count, fill=age)) +
         panel.background = element_blank(), 
         plot.title = element_text(size=12, face="bold", colour = "black"),
         axis.line = element_line(colour = "black"),
-        axis.title.x = element_blank(),
-        axis.text.x = element_blank(),
+        axis.title.x = element_text(size=14, face="bold", colour = "black"),
+        axis.text.x = element_text(size=12, colour = "black", vjust = 0.7, hjust=0.5),
         axis.title.y = element_text(size=14, face="bold", colour = "black"),
         legend.position = "bottom", 
         legend.title = element_text(size=14, face="bold", colour = "black"))
 
-perc <- ggplot(pubmed, aes(year, count, fill=age)) +
+perc <- ggplot(pubmed, aes(year, count, fill=publ)) +
   geom_bar(stat="identity", color="black", position = "fill") +
-  scale_fill_manual(values = c("non-development"= "#D55E00", "development" = "#009E73")) +
-  labs(x="Year", y="Publications (%)", fill="Age", title="B)") +
+  scale_fill_manual(values = c("Non-development"= "#D55E00", "Development" = "#009E73")) +
+  labs(x="Year", y="Publications (%)", fill="Publications type", title="B)") +
   scale_x_discrete(breaks=seq(min(as.numeric(levels(pubmed$year))),
                               max(as.numeric(levels(pubmed$year))),
                               10)) +
